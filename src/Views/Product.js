1f3e7ee1b608dc2a15d75c65a331122643bdfc9f
Tudor Andronachi <sorryy647@gmail.com>
2022-07-20 22:19:33 +0200

    last

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 
 
 function Product(props) {
-    // const url = 'https://pfp-public-productdb-api.azurewebsites.net/api/product/2'
-    const url = 'https://pfp-public-productdb-api.azurewebsites.net/api/picture/2'
+    const url = 'https://pfp-public-productdb-api.azurewebsites.net/api/product/5'
     const [product, setProduct] = useState(null)
 
     useEffect(() => {
@@ -18,7 +17,10 @@ function Product(props) {
         return(
             <div>
                 <div>
-                    <img src = {product}/> 
+                    <img src = {product.productPictures}/> 
+                </div>
+                <div>
+                    <h1 className='text-2xl font-bold mb-3'>{product.name}</h1>
                 </div>
             </div>  
         )
